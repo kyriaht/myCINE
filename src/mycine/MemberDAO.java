e1ad2ac401fa1c3396ce58d13aa463764c5bd578
kyriaht <kyriaht@naver.com>
2015-05-01 09:55:38 +0900

    형택이 커밋

---
+++ src/mycine/MemberDAO.java
@@ -0,0 +1,5 @@
+package mycine;
+
+public class MemberDAO {
+
+}
---
+++ src/mycine/MemberDTO.java
@@ -0,0 +1,20 @@
+package mycine;
+
+import java.sql.*;
+
+public class MemberDTO {
+	
+	private int idx;
+	private String id;
+	private String pwd;
+	private String pwd_ck;
+	private String name;
+	private String birth;
+	private String se;
+	private String tel;
+	private String genre;	//영화 장르
+	private String emailaddr;
+	
+	
+
+}
